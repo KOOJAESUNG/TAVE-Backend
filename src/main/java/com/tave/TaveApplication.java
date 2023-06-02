939e6fe434e8d1b65198b4138e6b5610087f5cd7
gujaeseong <gjs5448@gmail.com>
2023-06-03 02:16:12 +0900

    swagger /docs로 접근 가능하게 변경

--- src/main/java/com/tave/TaveApplication.java
+++ src/main/java/com/tave/TaveApplication.java
@@ -2,6 +2,14 @@ package com.tave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class TaveApplication {
@@ -10,9 +18,9 @@ public class TaveApplication {
         SpringApplication.run(TaveApplication.class, args);
     }
 
-//    @Bean
-//    RouterFunction<ServerResponse> routerFunction() {
-//        return route(GET("/docs"), req ->
-//                ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
-//    }
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return route(GET("/docs"), req ->
+                ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
+    }
 }
