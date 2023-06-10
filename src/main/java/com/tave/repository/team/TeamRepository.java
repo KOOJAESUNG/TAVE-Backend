7a35f674802a0b3b44fc299787faa68441f6d12f
gujaeseong <gjs5448@gmail.com>
2023-06-11 02:45:43 +0900

    query 메서드 추가

--- src/main/java/com/tave/repository/team/TeamRepository.java
+++ src/main/java/com/tave/repository/team/TeamRepository.java
@@ -2,8 +2,15 @@ package com.tave.repository.team;
 
 import com.tave.domain.team.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeamRepository extends JpaRepository<TeamEntity,Long> {
+
+    @Query("select t from TeamEntity t where t.id in :teamIds")
+    List<TeamEntity> findByIdIn(@Param("teamIds") List<Long> teamIds);
 }
