48937651798ea1cc0ff12291fcd9b0959b605991
gujaeseong <gjs5448@gmail.com>
2023-06-11 02:46:50 +0900

    MemberScore 관련 삭제

--- src/main/java/com/tave/repository/member/MemberScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tave.repository.member;
-
-import com.tave.domain.member.MemberScoreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberScoreRepository extends JpaRepository<MemberScoreEntity,Long> {
-}
