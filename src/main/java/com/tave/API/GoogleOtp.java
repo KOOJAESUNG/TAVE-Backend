ed2c0eb0dcba4cfc6139b7577656f60edd7339a7
Kimhojoon1 <jin96520@naver.com>
2023-06-28 10:21:50 +0900

    [GoogleOtp] API화 작업 완료

--- src/main/java/com/tave/API/GoogleOtp.java
+++ src/main/java/com/tave/API/GoogleOtp.java
@@ -8,24 +8,13 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base32;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GoogleOtp {
-
-    public static void main(String[] args) { //OTP 작동여부 확인 메소드, 나중에 삭제 예정
-        GoogleOtp otp = new GoogleOtp();
-        HashMap<String, String> map = otp.generate("name", "host");
-        String otpkey = map.get("encodedKey");
-        String url = map.get("url");
-        System.out.println(otpkey); // otp key 출력
-
-        Scanner sc = new Scanner(System.in);
-        String usercode = sc.nextLine();
-        // 생성된 키/url을 otp 앱에 등록 후 표시되는 번호와 생성된 키를 넣어주면 true
-        boolean check = otp.checkCode(usercode, otpkey);
-        System.out.println(check); // 키 일치여부 출력
-    }
-
-    public HashMap<String, String> generate(String userName, String hostName) {
+    @PostMapping("/googleOtp/generate")
+    public String generate() {
         HashMap<String, String> map = new HashMap<>();
         byte[] buffer = new byte[5 + 5 * 5];
         new Random().nextBytes(buffer);
@@ -34,15 +23,14 @@ public class GoogleOtp {
         byte[] bEncodedKey = codec.encode(secretKey);
 
         String encodedKey = new String(bEncodedKey); // 생성된 키
-        String url = getQRBarcodeURL(userName, hostName, encodedKey); // 생성된 QR코드
-        // Google OTP 앱에 userName@hostName 으로 저장됨
-        // key를 입력하거나 생성된 QR코드를 바코드 스캔하여 등록
 
         map.put("encodedKey", encodedKey);
-        map.put("url", url);
-        return map;
+        String otpkey = map.get("encodedKey");
+        
+        return otpkey;
     }
 
+    @PostMapping("/googleotp/checkCode")
     public boolean checkCode(String userCode, String otpkey) { // 키, 코드, 시간으로 일회용 비밀번호가 맞는지 확인
         long otpnum = Integer.parseInt(userCode); // Google OTP 앱에 표시되는 6자리 숫자
         long wave = new Date().getTime() / 30000; // Google OTP의 주기는 30초
@@ -85,11 +73,5 @@ public class GoogleOtp {
         truncatedHash %= 1000000;
 
         return (int) truncatedHash;
-    }
-
-    public static String getQRBarcodeURL(String user, String host, String secret) {
-        // QR코드 주소 생성
-        String format2 = "http://chart.apis.google.com/chart?cht=qr&chs=200x200&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s&chld=H|0";
-        return String.format(format2, user, host, secret);
     }
 }
