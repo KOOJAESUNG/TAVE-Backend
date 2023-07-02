d2200faf5098d330a1b7e4ddd0af5cc83b95863c
gujaeseong <gjs5448@gmail.com>
2023-07-02 17:48:43 +0900

    Swagger에서 JWT Token 헤더에 포함할 수 있도록 변경

--- src/main/java/com/tave/config/OpenApiConfig.java
+++ src/main/java/com/tave/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.tave.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,14 +13,40 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class OpenApiConfig {
+//    @Bean
+//    public OpenAPI openAPI() {
+//        Info info = new Info()
+//                .title("TAVE APP API Document")
+//                .version("v0.0.1");
+////                .description("");
+//        return new OpenAPI()
+//                .components(new Components())
+//                .info(info);
+//    }
+
     @Bean
     public OpenAPI openAPI() {
+
         Info info = new Info()
-                .title("TAVE APP API Document")
-                .version("v0.0.1");
-//                .description("");
+                .version("v1.0.0")
+                .title("API 타이틀")
+                .description("API Description");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "JWT Token";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer"));
+//                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
