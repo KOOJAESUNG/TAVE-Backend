c56f93182139f4340f14a8b13e7af09d8fdc9e6a
gujaeseong <gjs5448@gmail.com>
2023-07-11 05:57:13 +0900

    getMembersAllMemberScoreNote 구현

--- src/main/java/com/tave/repository/admin/MemberScoreNoteRepository.java
+++ src/main/java/com/tave/repository/admin/MemberScoreNoteRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,5 @@ public interface MemberScoreNoteRepository extends JpaRepository<MemberScoreNote
     @Query("select sum(m.score) from MemberScoreNoteEntity m where m.member.id = :memberId")
     Optional<Integer> getMemberScoreByMemberId(@Param("memberId") Long memberId);
 
+    Optional<List<MemberScoreNoteEntity>> findMemberScoreNoteEntitiesByMemberId(Long memberId);
 }
--- src/main/java/com/tave/service/admin/MemberScoreNoteService.java
+++ src/main/java/com/tave/service/admin/MemberScoreNoteService.java
@@ -13,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -22,33 +25,44 @@ public class MemberScoreNoteService {
     private final MemberScoreNoteRepository memberScoreNoteRepository;
     private final MemberRepository memberRepository;
     private final MemberScoreNoteMapper memberScoreNoteMapper;
+
     @Transactional
     public MemberScoreNoteDto.MemberScoreNoteResponseDto createMemberScoreNote(MemberScoreNoteDto.MemberScoreNotePostDto memberScoreNotePostDto) {
         MemberEntity memberEntity = memberRepository.findById(memberScoreNotePostDto.getMemberId())
-                .orElseThrow(()->new BusinessLogicException(ExceptionCode.CREATE_FAIL_MEMBERSCORENOTE));
-        return memberScoreNoteMapper.toResponseDto(memberScoreNoteRepository.save(memberScoreNoteMapper.toEntity(memberScoreNotePostDto,memberEntity)));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.CREATE_FAIL_MEMBERSCORENOTE));
+        return memberScoreNoteMapper.toResponseDto(memberScoreNoteRepository.save(memberScoreNoteMapper.toEntity(memberScoreNotePostDto, memberEntity)));
     }
 
     public MemberScoreNoteDto.MemberScoreNoteResponseDto getMemberScoreNote(Long memberScoreNoteId) {
         return memberScoreNoteMapper.toResponseDto(memberScoreNoteRepository.findById(memberScoreNoteId)
-                .orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBERSCORENOTE_IS_NOT_EXIST)));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBERSCORENOTE_IS_NOT_EXIST)));
     }
 
     @Transactional
     public MemberScoreNoteDto.MemberScoreNoteResponseDto updateMemberScoreNote(MemberScoreNoteDto.MemberScoreNotePatchDto memberScoreNotePatchDto) {
         MemberScoreNoteEntity memberScoreNoteEntity = memberScoreNoteRepository.findById(memberScoreNotePatchDto.getId())
-                .orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBERSCORENOTE_IS_NOT_EXIST));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBERSCORENOTE_IS_NOT_EXIST));
         //update
 //        memberScoreNoteEntity.updateFromPatchDto(memberScoreNotePatchDto);
-        memberScoreNoteMapper.updateFromPatchDto(memberScoreNotePatchDto,memberScoreNoteEntity);
+        memberScoreNoteMapper.updateFromPatchDto(memberScoreNotePatchDto, memberScoreNoteEntity);
         //entity->dto 후 return
         return memberScoreNoteMapper.toResponseDto(memberScoreNoteRepository.findById(memberScoreNotePatchDto.getId())
-                .orElseThrow(()->new BusinessLogicException(ExceptionCode.UPDATE_FAIL_ADMIN)));
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.UPDATE_FAIL_ADMIN)));
     }
 
     @Transactional
     public void deleteMemberScoreNote(Long memberScoreNoteId) {
         memberScoreNoteRepository.deleteById(memberScoreNoteId);
         log.info("MemberScoreNoteEntity Id: {} is deleted", memberScoreNoteId);
+    }
+
+    public List<MemberScoreNoteDto.MemberScoreNoteResponseDto> getMembersAllMemberScoreNote(Long memberId) {
+        List<MemberScoreNoteDto.MemberScoreNoteResponseDto> list = new ArrayList<>();
+        memberScoreNoteRepository.findMemberScoreNoteEntitiesByMemberId(memberId).
+                orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBERSCORENOTE_IS_NOT_EXIST)).
+                forEach(m ->
+                    list.add(memberScoreNoteMapper.toResponseDto(m))
+                );
+        return list;
     }
 }
